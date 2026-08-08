0535598cf4e6d779e1f63608350ccc667635749b
jeellakhani <jeellakhani2006@gmail.com>
2026-08-08 10:36:57 +0530

    Add ADivisibilityProblem

---
+++ ADivisibilityProblem.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ADivisibilityProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Read number of test cases
+        
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            if (a % b == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(b - (a % b));
+            }
+        }
+        sc.close();
+    }
+}
