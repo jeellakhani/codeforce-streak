1aa6652da9fc78b508dc9b57e2388286a6bd6f56
jeellakhani <jeellakhani2006@gmail.com>
2026-08-08 10:36:59 +0530

    Add AUltraFastMathematician

---
+++ AUltraFastMathematician.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class AUltraFastMathematician {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                result.append('1');
+            } else {
+                result.append('0');
+            }
+        }
+        System.out.println(result.toString());
+        sc.close();
+    }
+}
