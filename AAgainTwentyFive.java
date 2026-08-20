aab87b4054ffb02965b1bbd8b805a9e57c0dc39a
jeellakhani <jeellakhani2006@gmail.com>
2026-08-20 07:58:16 +0530

    Add AAgainTwentyFive

---
+++ AAgainTwentyFive.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class AAgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextLong()) {
+            long n = scanner.nextLong();
+            System.out.println(25);
+        }
+        scanner.close();
+    }
+}
