c377e1f06eaa955342d5871e074c7fa2c2635fb7
jeellakhani <jeellakhani2006@gmail.com>
2026-08-24 23:27:37 +0530

    Add ADivision

---
+++ ADivision.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ADivision {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int rating = sc.nextInt();
+            if (rating >= 1900) {
+                System.out.println("Division 1");
+            } else if (rating >= 1600) {
+                System.out.println("Division 2");
+            } else if (rating >= 1400) {
+                System.out.println("Division 3");
+            } else {
+                System.out.println("Division 4");
+            }
+        }
+        sc.close();
+    }
+}
